e2e991bfcad85672b1101573a74bdaf3fca3ac08
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-25 17:18:25 +0200

    tableView to ListViewController

--- RSSFeedReader/View/ListViewController.swift
+++ RSSFeedReader/View/ListViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 class ListViewController: UIViewController {
-
+  var tableView: UITableView!
   var viewModel: ListViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
+      title = "Title"
+      showTableView()
     }
+  private func showTableView() {
+    tableView = UITableView(frame: view.frame)
+    tableView.dataSource = self
+    view.addSubview(tableView)
+  }
+}
+// MARK: - UITableViewDataSource
+extension ListViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "ListCell")
+    cell.textLabel?.text = "Text Cell"
+    return cell
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 30  }
 }
