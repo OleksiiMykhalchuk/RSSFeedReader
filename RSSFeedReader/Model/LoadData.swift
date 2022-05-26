0ce2a20332999501e497eb1fea651ed41a3e09d4
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-26 11:20:07 +0200

    get data and save into dictionary array

--- RSSFeedReader/Model/LoadData.swift
+++ RSSFeedReader/Model/LoadData.swift
@@ -15,35 +15,50 @@ class LoadData: NSObject {
     let url = URL(string: urlString)!
     let session = URLSession.shared
     let dataTask = session.dataTask(with: url) { data, response, error in
-      print(error)
+      self.parse(data: data!)
     }
     dataTask.resume()
   }
-  func parse(data: Data) -> [Result] {
+  func parse(data: Data) {
       let parser = XMLParser(data: data)
       parser.delegate = self
       parser.parse()
-    return [Result]()
   }
+  var results: [[String: String]]?
+  var currentDictionary: [String: String]?
+  var currentValue: String?
+  var rssChannel = ["rss", "channel"]
+  var dictionaryKeys = ["title", "link", "description", "language", "pubDate", "copyright", "docs", "generator", "manageingEditor", "image", "url"]
+
 }
 extension LoadData: XMLParserDelegate {
 
   func parserDidStartDocument(_ parser: XMLParser) {
-
+    results = []
   }
   func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-
+    if "item" == elementName {
+      currentDictionary = [:]
+    } else if dictionaryKeys.contains(elementName) {
+      currentValue = ""
+    }
   }
   func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-
+    if "item" == elementName {
+      results?.append(currentDictionary!)
+      currentDictionary = nil
+    } else if dictionaryKeys.contains(elementName) {
+      currentDictionary?[elementName] = currentValue
+      currentValue = nil
+    }
   }
   func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
-
+    print("Parse Error \(parseError)")
   }
   func parserDidEndDocument(_ parser: XMLParser) {
-
+    print(results)
   }
   func parser(_ parser: XMLParser, foundCharacters string: String) {
-
+    currentValue? += string
   }
 }
