4cf9465384324652b0fb7489638384ab139ea01e
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-06-19 16:32:31 +0900

    ListDetails

---
+++ RSSFeedReader/Flows/Settings/Details/LinkDetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LinkDetailsViewController.swift
+//  RSSFeedReader
+//
+//  Created by Oleksii Mykhalchuk on 6/19/22.
+//
+
+import UIKit
+
+class LinkDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RSSFeedReader/Flows/Settings/Details/ListDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListDetailsViewModel.swift
+//  RSSFeedReader
+//
+//  Created by Oleksii Mykhalchuk on 6/19/22.
+//
+
+import Foundation
